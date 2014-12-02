13b14933e5fee480bbfcef6e3695377ed28ddffd
Dorin Cohen <dorinco.91@gmail.c>
2014-12-02 22:00:37 +0200

    exercise5- portfolio

---
+++ src/com/mta/javacourse/model/Portfolio.java
@@ -0,0 +1,79 @@
+package com.mta.javacourse.model;
+
+import java.util.Date;
+
+import com.mta.javacourse.Stock;
+public class Portfolio {
+
+	 private final static int MAX_PORTFOLIO_SIZE=5;
+	 String title;
+	 private Stock[] stocks;
+	 private StockStatus[] stockStatus;
+	int portfolioSize=0;
+	 //constractors 
+	public Portfolio()
+	{
+		 stockStatus=new StockStatus[MAX_PORTFOLIO_SIZE];
+		 stocks=new Stock[MAX_PORTFOLIO_SIZE];
+	}
+	 
+	 //setters
+	public void setStocks(Stock[] stocks1)
+	{
+		stocks=stocks1;
+	}
+	public void setTitle(String title1)
+	{
+		title=title1;
+	}
+	public void setStockStatus(StockStatus[] stockStatus1)
+	{
+		stockStatus=stockStatus1;
+	}
+
+	//getters 
+	 public Stock[] getStocks()
+	 {
+		 return stocks;
+	 }
+	 public String getTitle()
+	 {
+		 return title;
+	 }
+	 
+	//methods/
+	 public void addStock(Stock s)
+	 {
+			 stocks[portfolioSize] = s;
+			 portfolioSize++;
+		 
+	 }
+
+	 public String getHtmlString() 
+	 {
+		
+		String stockStr;
+		
+		String s1String=stocks[0].getHtmlDescription();
+		String s2String=stocks[1].getHtmlDescription();
+		String s3String=stocks[2].getHtmlDescription();
+		
+		stockStr="<h1>"+title+"</h1>"+"<br>"+s1String+"<br>"+s2String+"<br>"+s3String;
+		return stockStr;
+	 }
+	 //inner class stockStatus
+	 public class StockStatus {
+			private final static int DO_NOTHING = 0;
+			private final static int BUY = 1;
+			private final static int SELL = 2;	
+					
+			String symbol;
+			float currentBid,currentAsk;
+			Date date;
+			int recommendation;
+			int stockQuantity;	
+		}
+	 
+	 
+}
+
